a192e0b046388e7ff5462cf678d42a3910886f9d
dristosun <107073369+dristosun@users.noreply.github.com>
2023-02-22 16:05:22 +0200

    Add functions to update the grid model and the paragraph with the number of blocks

--- index.js
+++ index.js
@@ -1,18 +1,39 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const val = document.querySelector('.rangeValue').value;
+
+const DEFAULT_VALUE = 16;
+const DEFAULT_BACKGROUND = 'white';
+
+let val = DEFAULT_VALUE;
+
+const rangeSlider = document.querySelector('.rangeSlider');
+const rangeLabel = document.querySelector('.rangeLabel');
+const grid = document.querySelector('.grid');
+
+
+rangeSlider.addEventListener('change', () => {updateGrid();});
+rangeSlider.addEventListener('mousemove', () => {updateRangeLabel();});
+rangeSlider.addEventListener('touchmove', () => {updateRangeLabel();});
+
+
+function updateRangeLabel() {
+    const val = rangeSlider.value;
+    rangeLabel.textContent = `${val} x ${val}`;
+}
+
+function updateGrid() {
+    grid.innerHTML = '';
+    const val = rangeSlider.value;
+    grid.style.gridTemplateColumns = `repeat(${val}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${val}, 1fr)`;
+    for (let i = 1; i <= val * val; i++) {
+        console.log(i);
+        const cell = document.createElement('div');
+        cell.className = 'cell';
+        grid.appendChild(cell);
+    }
+}
+
+window.onload = () => {
     updateRangeLabel(val);
     updateGrid(val);
-    document.querySelector('.rangeValue').addEventListener('input', function() {
-        const val = document.querySelector('.rangeValue').value;
-        updateRangeLabel(val);
-        updateGrid(val);
-    })
-})
-
-function updateRangeLabel(value) {
-        document.querySelector('.rangeLabel').textContent = `${value} x ${value}`;
 }
 
-function updateGrid(val) {
-    console.log(val);
-}
