d4f9fd109f02b485c3d1710494f1d5e2d997d054
dristosun <107073369+dristosun@users.noreply.github.com>
2023-02-22 19:11:06 +0200

    Add mobile functionality for drawing

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 
 const DEFAULT_VALUE = 16;
 const DEFAULT_BACKGROUND = 'white';
+const DEFAULT_DRAW_COLOR = 'black';
 
 let val = DEFAULT_VALUE;
+let drawColor = DEFAULT_DRAW_COLOR;
 
 const rangeSlider = document.querySelector('.rangeSlider');
 const rangeLabel = document.querySelector('.rangeLabel');
@@ -10,7 +12,7 @@ const grid = document.querySelector('.grid');
 let cells = document.querySelectorAll('.cell');
 
 
-rangeSlider.addEventListener('change', () => {updateGrid(); clickChangeColor();});
+rangeSlider.addEventListener('change', () => {updateGrid(); draw();});
 rangeSlider.addEventListener('mousemove', () => {updateRangeLabel();});
 rangeSlider.addEventListener('touchmove', () => {updateRangeLabel();});
 
@@ -32,21 +34,31 @@ function updateGrid() {
     }
 }
 
-function clickChangeColor() {
-    let cells = document.querySelectorAll('.cell');
+function draw() {
+    const cells = document.querySelectorAll('.cell');
     for (let i = 0; i < cells.length; i++) {
         cells[i].addEventListener('mousemove', (e) => {
             if (e.buttons === 1) {
                 cells[i].style.backgroundColor = 'black';
             }
         })
+        cells[i].addEventListener('touchmove', (e) => {
+            var myLocation = event.touches[0];
+            var realTarget = document.elementFromPoint(myLocation.clientX, myLocation.clientY);
+            cells.forEach(cell => {
+                if (realTarget === cell) {
+                    cell.style.backgroundColor = 'black';
+                }
+            })
+        })
     }
 }
+
+
 
 window.onload = () => {
     updateRangeLabel(val);
     updateGrid(val);
-    clickChangeColor();
+    draw();
 }
 
-lala
