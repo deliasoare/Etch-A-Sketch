c058c30483b5cefedb2d1170a1a21d671c46a305
dristosun <107073369+dristosun@users.noreply.github.com>
2023-02-22 18:24:31 +0200

    Add coloring function - basic black on white

--- index.js
+++ index.js
@@ -7,9 +7,10 @@ let val = DEFAULT_VALUE;
 const rangeSlider = document.querySelector('.rangeSlider');
 const rangeLabel = document.querySelector('.rangeLabel');
 const grid = document.querySelector('.grid');
+let cells = document.querySelectorAll('.cell');
 
 
-rangeSlider.addEventListener('change', () => {updateGrid();});
+rangeSlider.addEventListener('change', () => {updateGrid(); clickChangeColor();});
 rangeSlider.addEventListener('mousemove', () => {updateRangeLabel();});
 rangeSlider.addEventListener('touchmove', () => {updateRangeLabel();});
 
@@ -25,15 +26,26 @@ function updateGrid() {
     grid.style.gridTemplateColumns = `repeat(${val}, 1fr)`;
     grid.style.gridTemplateRows = `repeat(${val}, 1fr)`;
     for (let i = 1; i <= val * val; i++) {
-        console.log(i);
         const cell = document.createElement('div');
         cell.className = 'cell';
         grid.appendChild(cell);
     }
 }
 
+function clickChangeColor() {
+    let cells = document.querySelectorAll('.cell');
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].addEventListener('mousemove', (e) => {
+            if (e.buttons === 1) {
+                cells[i].style.backgroundColor = 'black';
+            }
+        })
+    }
+}
+
 window.onload = () => {
     updateRangeLabel(val);
     updateGrid(val);
+    clickChangeColor();
 }
 
